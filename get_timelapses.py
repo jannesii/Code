4dfbbda4dbcf406e6d10c8bd7c1b90f5de0ac613
jannesii <janne.siirtola@outlook.com>
2025-02-19 15:49:14 +0200

    refactor remote folder handling and update video codec to avc1 in timelapse script

--- get_timelapses.py
+++ get_timelapses.py
@@ -7,7 +7,13 @@ RASPI_HOST = '192.168.1.128'
 RASPI_PORT = 22
 USERNAME = 'jannesi'
 PASSWORD = 'jannesi'  # Use key-based authentication in production
-REMOTE_FOLDERS = ["/home/jannesi/Code/testPhotos/", "/home/jannesi/Code/Timelapses/"]
+REMOTE_FOLDERS = []
+testPhotos = "/home/jannesi/Code/testPhotos/"
+timelapses = "/home/jannesi/Code/Timelapses/"
+testtimelapses = "/home/jannesi/Code/testTimelapses/"
+#REMOTE_FOLDERS.append(testPhotos)
+REMOTE_FOLDERS.append(timelapses)
+#REMOTE_FOLDERS.append(testtimelapses)
 
 POLL_INTERVAL = 10  # seconds
 
@@ -24,12 +30,15 @@ def transfer_files():
             for file_name in remote_files:
                 remote_file_path = os.path.join(REMOTE_FOLDER, file_name)
                 local_file_path = os.path.join(os.getcwd(), REMOTE_FOLDER.split("/")[-2] ,file_name)
+                #print(local_file_path, " | ", remote_file_path)
                 print(f"Transferring {file_name}...")
                 sftp.get(remote_file_path, local_file_path)
+                print(f"{file_name} transferred to local machine.")
                 
                 # Remove remote file after successful download
+                print(f"Deleting {file_name} on Raspberry Pi...")
                 sftp.remove(remote_file_path)
-                print(f"{file_name} transferred and deleted on Raspberry Pi.\n")
+                print(f"{file_name} deleted on Raspberry Pi.\n")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
@@ -39,5 +48,6 @@ def transfer_files():
 if __name__ == '__main__':
     while True:
         transfer_files()
+        break
         time.sleep(POLL_INTERVAL)
         
