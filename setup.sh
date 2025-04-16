6b8d46897a3ae376b573b6af317e3432151be2ca
jannesii <janne.siirtola@outlook.com>
2025-04-16 17:34:34 +0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade
+sudo apt install libcap-dev libatlas-base-dev ffmpeg libopenjp2-7
+sudo apt install libcamera-dev
+sudo apt install libkms++-dev libfmt-dev libdrm-dev
