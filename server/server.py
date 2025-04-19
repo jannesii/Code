5781ba56c10c40627c518a073ad1223eda61cfe0
jannesii <janne.siirtola@outlook.com>
2025-04-19 19:48:59 +0300

    no https

--- server/server.py
+++ server/server.py
@@ -153,5 +153,4 @@ if __name__ == '__main__':
                  host='0.0.0.0', 
                  port=5555, 
                  debug=True,
-                 ssl_context=('cert.pem','key.pem')
                  )
