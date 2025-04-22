1f52c6ac0f5b1678704c3c7f6a4af0eb9a7f46ee
jannesii <janne.siirtola@outlook.com>
2025-04-22 15:48:21 +0300

    Add adafruit-circuitpython-dht to requirements and update setup script for improved installation

--- client/setup.sh
+++ client/setup.sh
@@ -2,16 +2,16 @@ sudo apt update -y
 sudo apt install -y libcap-dev libatlas-base-dev ffmpeg libopenjp2-7
 sudo apt install -y libcamera-dev
 sudo apt install -y libkms++-dev libfmt-dev libdrm-dev
-sudo apt install -y python3-pip python3-dev build-essential
+sudo apt install -y python3-pip python3-dev build-essential libgpiod2
 
 python -m venv --system-site-packages .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git temp‑dht
-cd temp‑dht
-python setup.py install --force-pi
-cd ..
-sudo rm -rf temp‑dht
+#git clone https://github.com/adafruit/Adafruit_Python_DHT.git temp‑dht
+#cd temp‑dht
+#python setup.py install --force-pi
+#cd ..
+#sudo rm -rf temp‑dht
 
