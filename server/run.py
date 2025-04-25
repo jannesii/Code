f18c371a0206095bdc978c856f9c4cc3c47d6a7f
jannesii <janne.siirtola@outlook.com>
2025-04-25 19:06:14 +0300

    Refactor logging setup in run.py to ensure debug information is logged correctly when FLASK_DEBUG is set

--- server/run.py
+++ server/run.py
@@ -7,11 +7,15 @@ from app import create_app
 app, socketio = create_app()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     debug = os.getenv("FLASK_DEBUG")
+    logging.info(f"DEBUG: {debug}")
     if debug:
         logging.getLogger("werkzeug").setLevel(logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Debug mode enabled")
         logging.warning("Debug mode enabled")
+    else:
+        logging.basicConfig(level=logging.INFO)
         
     socketio.run(
         app,
